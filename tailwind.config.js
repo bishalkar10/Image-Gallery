0438462934ae60c438fb98d93e4439afe59a80a4
Bishal Kar <bishalkar10@gmail.com>
2023-05-09 16:19:40 +0530

    reduced welcome page animation time

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,20 @@ module.exports = {
     extend: { 
       keyframes: { 
         logo : {
-          '0%' : { opacity: '0', transform : 'translateY(-70%)'},
+          '0%' : { opacity: '0', transform : 'translateY(-100%)'},
+          '70%' : { opacity: '0'},
           
-          '60%' : { opacity: '0', transform : 'translateY(-70%)'},
-          '90%' : { opacity: '1', transform : 'translateY(0)'},
           '100%' : { opacity : '1', transform : 'translateY(0)'}
         }, 
         welcomeTo : {
-          '0%' : { opacity: '0'},  
+          '0%' : { opacity: '0'},     
           '100%' : { opacity : '1'}
         }, 
       }, 
       animation : { 
-        'to' : 'welcomeTo 3.5s linear',
-        'welcome-to' : 'welcomeTo 3s linear', 
-        'logo-animation' : 'logo 8s linear -3s',
+        'to' : 'welcomeTo 2.5s linear',
+        'welcome-to' : 'welcomeTo 2s linear', 
+        'logo-animation' : 'logo 5s linear -2s',
       }, 
     },
   },
