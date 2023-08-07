62245c4b2fbcf0a426a96c3beca81390d68129af
Bishal Kar <bishalkar10@gmail.com>
2023-08-08 04:37:22 +0530

    Removed a space between {html, js}

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html, js}'],
-  
-  theme: { 
-    fontFamily : {
-      'BreeSerif' : ['Bree Serif', 'serif'],
-      'BrunoAceSC' : ['Bruno Ace SC', 'cursive'],
-      'Roboto' : ['Roboto', 'sans serif'],
-    }, 
-    extend: { 
-      keyframes: { 
-        logo : {
-          '0%' : { opacity: '0', transform : 'translateY(-100%)'},
-          '70%' : { opacity: '0'},
-          
-          '100%' : { opacity : '1', transform : 'translateY(0)'}
-        }, 
-        welcomeTo : {
-          '0%' : { opacity: '0'},     
-          '100%' : { opacity : '1'}
-        }, 
-      }, 
-      animation : { 
-        'to' : 'welcomeTo 2.5s linear',
-        'welcome-to' : 'welcomeTo 2s linear', 
-        'logo-animation' : 'logo 5s linear -2s',
-      }, 
+  content: ["./*.{html,js}"],
+
+  theme: {
+    fontFamily: {
+      BreeSerif: ["Bree Serif", "serif"],
+      BrunoAceSC: ["Bruno Ace SC", "cursive"],
+      Roboto: ["Roboto", "sans serif"],
+    },
+    extend: {
+      keyframes: {
+        logo: {
+          "0%": { opacity: "0", transform: "translateY(-100%)" },
+          "70%": { opacity: "0" },
+
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        welcomeTo: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        to: "welcomeTo 2.5s linear",
+        "welcome-to": "welcomeTo 2s linear",
+        "logo-animation": "logo 5s linear -2s",
+      },
     },
   },
   plugins: [],
-}
+};
